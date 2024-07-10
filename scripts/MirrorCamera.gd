bca3d6f10aaa6c01ffc970758f1f40450b3f93da
Cafezinhu <aertoncastro@gmail.com>
2024-07-10 07:26:38 -0300

    fix mirror tilt

--- scripts/MirrorCamera.gd
+++ scripts/MirrorCamera.gd
@@ -2,35 +2,33 @@ extends Camera3D
 
 @export var xr_camera: XRCamera3D
 var mirror: MeshInstance3D
+enum Eye {Left, Right}
+@export var eye: Eye
+var eye_position: Marker3D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	mirror = get_parent().get_parent()
-	var n = mirror.global_basis.z
-	var p = mirror.global_position
-	var d = -(n.x * p.x + n.y * p.y + n.z * p.z)
-	print(mirror.global_basis.z)
-	print(mirror.global_position)
-	print(p.length())
-	print(d)
-	pass # Replace with function body.
-
+	if eye == Eye.Left:
+		eye_position = xr_camera.get_node("LeftEye")
+	else:
+		eye_position = xr_camera.get_node("RightEye")
 #func p
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	mirror.rotation_degrees.z = 0
-	var xr_camera_pos = Vector3(-xr_camera.global_position.x, 0, xr_camera.global_position.z)
+
 	var mirror_pos = Vector3(mirror.global_position.x, 0, mirror.global_position.z)
 
-	var dir: Vector3 = xr_camera.global_basis.z - 2 * (xr_camera.global_basis.z.dot(mirror.global_basis.z)) * mirror.global_basis.z
+	var dir: Vector3 = eye_position.global_basis.z - 2 * (eye_position.global_basis.z.dot(mirror.global_basis.z)) * mirror.global_basis.z
 	
-	look_at(dir + global_position, mirror.global_basis.y, true)
+	look_at(dir + global_position, mirror.global_basis.y.rotated(mirror.global_basis.z, eye_position.global_rotation.z), true)
 	
 	var n = -mirror.global_basis.z
 	var p = mirror.global_position
 	var d = -(n.x * p.x + n.y * p.y + n.z * p.z)
 	
-	var po = xr_camera.global_position
+	var po = eye_position.global_position
 	
 	var t = (-d - n.x * po.x - n.y * po.y - n.z * po.z)/(n.x ** 2 + n.y ** 2 + n.z ** 2)
 	
@@ -39,4 +37,5 @@ func _process(delta):
 	var z = po.z + n.z * t * 2
 	
 	global_position = Vector3(x, y, z)
+	#print(global_position)
 	near = t
