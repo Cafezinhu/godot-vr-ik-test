9d7869deccdc1489a2084ea3bfb852770d6c2592
Cafezinhu <aertoncastro@gmail.com>
2024-07-10 06:26:39 -0300

    mirror camera near

--- scripts/MirrorCamera.gd
+++ scripts/MirrorCamera.gd
@@ -2,7 +2,6 @@ extends Camera3D
 
 @export var xr_camera: XRCamera3D
 var mirror: MeshInstance3D
-@onready var bola: MeshInstance3D = $"../../Bola"
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	mirror = get_parent().get_parent()
@@ -19,19 +18,13 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	mirror.rotation_degrees.z = 0
 	var xr_camera_pos = Vector3(-xr_camera.global_position.x, 0, xr_camera.global_position.z)
 	var mirror_pos = Vector3(mirror.global_position.x, 0, mirror.global_position.z)
-	#global_position = (mirror_pos - xr_camera_pos).rotated(Vector3.UP, mirror.global_rotation.y ** 2) + mirror_pos
-	
-	#global_rotation_degrees.x = xr_camera.global_rotation_degrees.x
-		#
-	#global_rotation_degrees.y = -(xr_camera.global_rotation_degrees.y + 180 - mirror.global_rotation_degrees.y)
-	#global_rotation_degrees.z = -xr_camera.global_rotation_degrees.z
-	
-	#TODO: colocar mirror 3d no look at
+
 	var dir: Vector3 = xr_camera.global_basis.z - 2 * (xr_camera.global_basis.z.dot(mirror.global_basis.z)) * mirror.global_basis.z
 	
-	look_at(dir + global_position, Vector3.UP, true)
+	look_at(dir + global_position, mirror.global_basis.y, true)
 	
 	var n = -mirror.global_basis.z
 	var p = mirror.global_position
@@ -45,6 +38,5 @@ func _process(delta):
 	var y = po.y + n.y * t * 2
 	var z = po.z + n.z * t * 2
 	
-	bola.global_position = Vector3(x, y, z)
-	
 	global_position = Vector3(x, y, z)
+	near = t
